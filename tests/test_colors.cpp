785675fb69bdd540eb75c9d18e719afdc7b7f921
Thomas Cohn <cohnt@umich.edu>
2019-01-01 15:54:51 -0500

    Test printing colors out to the terminal.

--- tests/test_colors.cpp
+++ tests/test_colors.cpp
@@ -5,6 +5,23 @@
 
 int main() {
 	std::cout << Colors::colorToTermCode.at(Colors::red) << "red" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::blue) << "blue" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::yellow) << "yellow" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::green) << "green" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::orange) << "orange" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::cyan) << "cyan" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::magenta) << "magenta" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::maroon) << "maroon" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::purple) << "purple" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::gray) << "gray" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::white) << "white" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::bright_green) << "bright_green" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::tan) << "tan" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::dark_blue) << "dark_blue" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::dark_cyan) << "dark_cyan" << std::endl;
+	std::cout << Colors::colorToTermCode.at(Colors::pink) << "pink" << std::endl;
+
+	std::cout << Colors::reset;
 
 	return 0;
 }
