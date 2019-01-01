f09c72b5979fd553b206d15d28a1b50cf0f23bcd
Thomas Cohn <cohnt@umich.edu>
2019-01-01 17:16:39 -0500

    Tool to print out a given state file.

--- tools/print_state_file.cpp
+++ tools/print_state_file.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <fstream>
 
+#include "../game_state.h"
+
 int main(int argc, char** argv) {
 	if(argc != 2) {
 		std::cout << "Usage: ./print_state_file [filename]" << std::endl;
+		exit(1);
 	}
 
 	std::ifstream in(argv[1]);
+	int rows, cols;
+
+	in >> rows;
+	in >> cols;
+
+	std::vector<std::string> lines;
+	std::string line;
+	while(std::getline(in >> std::ws, line)) {
+		lines.push_back(line);
+	}
+
+	State state(rows, cols, lines);
+
+	state.print();
 
 	in.close();
 
