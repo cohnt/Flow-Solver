7cb46def692909712bd4a78efe9a19bffb49fdf0
Thomas Cohn <cohnt@umich.edu>
2019-01-01 02:24:26 -0500

    Test the Node constructors and destructors.

---
+++ tree/tests.cpp
@@ -0,0 +1,22 @@
+#include "tree.h"
+
+int main() {
+	//Test the various Node constructors available.
+	Tree<int>::Node* n1 = new Tree<int>::Node();
+	Tree<int>::Node* n2 = new Tree<int>::Node(10);
+	Tree<int>::Node* n3 = new Tree<int>::Node(n2);
+	Tree<int>::Node* n4 = new Tree<int>::Node(n2, 20);
+	Tree<int>::Node* n5 = new Tree<int>::Node(n2, 30);
+	Tree<int>::Node* n6 = new Tree<int>::Node(n4, 40);
+
+	//Suppress the unused variable warning -- they are being used.
+	(void)n3;
+	(void)n5;
+	(void)n6;
+
+	//Run with valgrind to ensure no memory is leaked
+	delete n1;
+	delete n2;
+
+	return 0;
+}
