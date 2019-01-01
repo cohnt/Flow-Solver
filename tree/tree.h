7588fe4ea44e260ad0743a5aab02c28aa183ac2f
Thomas Cohn <cohnt@umich.edu>
2019-01-01 01:46:24 -0500

    List of definitions needed for the tree class.

---
+++ tree/tree.h
@@ -0,0 +1,46 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include <cstddef>
+#include <list>
+
+template <typename T> class Tree {
+public:
+	struct Node {
+		friend class Tree;
+	private:
+		std::list<Node*> children;
+		Node* parent;
+		T data;
+	public:
+		T& operator*();
+	};
+
+private:
+	Node* root;
+	size_t size;
+	size_t height;
+
+public:
+	Tree(T);
+	~Tree();
+
+	size_t getSize() const;
+	size_t getHeight() const;
+
+	Node* getRoot() const;
+	Node* addChild(Node*, T);
+	
+	bool deleteFirstChild(Node*);
+	bool deleteLastChild(Node*);
+	bool deleteNthChild(Node*, size_t n);
+
+	Node* getParent(Node*) const;
+
+	size_t numChildren(Node*) const;
+	Node* firstChild(Node*) const;
+	Node* lastChild(Node*) const;
+	Node* nthChild(Node*, size_t n) const;
+};
+
+#endif
